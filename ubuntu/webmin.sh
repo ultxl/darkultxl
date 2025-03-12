10219efdeff7c54b23f59297bcb8b06714573194
AmTevi <140701204+ultxl@users.noreply.github.com>
2025-03-13 00:24:53 +0300

    Update webmin.sh

--- ubuntu/webmin.sh
+++ ubuntu/webmin.sh
@@ -43,8 +43,8 @@ export WARNING="${RED}\e[5m"
 export UNDERLINE="\e[4m"
 
 # // Exporting URL Host
-export Server_URL="raw.githubusercontent.com/NevermoreSSH/Blueblue/main/test"
-export Server1_URL="raw.githubusercontent.com/NevermoreSSH/Blueblue/main/limit"
+export Server_URL="raw.githubusercontent.com/ultxl/darkultxl/main/test"
+export Server1_URL="raw.githubusercontent.com/ultxl/darkultxl/main/limit"
 export Server_Port="443"
 export Server_IP="underfined"
 export Script_Mode="Stable"
